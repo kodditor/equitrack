7f7bd2172fa43062514a222dcca8c63489c88533
Kwabena Owusu-Darko <kobbyowusudarko@gmail.com>
2024-01-22 23:26:45 +0000

    Added page paths

--- frontend/components/header-component.tsx
+++ frontend/components/header-component.tsx
@@ -9,10 +9,10 @@ export default function Header(){
         <>
             <div className="header-gradient z-20"></div>
             <nav className="w-[calc(100%-18rem)] z-50 border-2 border-gray-700 ml-[9rem] flex justify-between bg-black px-5 py-2 fixed top-5 m-auto items-center rounded-full">    
-                <Image src={'/logo-white.webp'} width={100} height={30} alt="The Equitrack Logo" priority/>
+                <Link href={'/'}><Image src={'/logo-white.webp'} width={100} height={30} alt="The Equitrack Logo" priority/></Link>
                 <ul className="hidden md:flex gap-4">
-                    <Link href={''} className="text-white hover:text-green" ><li>All Stocks</li></Link>
-                    <Link href={''} className="text-white hover:text-green" ><li>My Stocks</li></Link>
+                    <Link href={'/'} className="text-white hover:text-green" ><li>All Stocks</li></Link>
+                    <Link href={'/my-stocks'} className="text-white hover:text-green" ><li>My Stocks</li></Link>
                 </ul>
             </nav>
         </>
