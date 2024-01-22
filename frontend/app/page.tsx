d17fb38b27427e797494b22489a275046134baf9
Kwabena Owusu-Darko <kobbyowusudarko@gmail.com>
2024-01-22 23:25:49 +0000

    Added Market news to homepage

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,5 +1,6 @@
 import Footer from '@/components/footer-component'
 import Header from '@/components/header-component'
+import MarketNewsList from '@/components/market-news-component'
 import StocksList from '@/components/stockslist-component'
 import Image from 'next/image'
 
@@ -11,8 +12,11 @@ export default function Home() {
 			<Header />
 			<main className='min-h-screen w-full px-12 pt-24 md:px-36 text-white'>
 				<section>
-					<div>
+					<div className='mb-8'>
 						<StocksList />
+					</div>
+					<div>
+						<MarketNewsList />
 					</div>
 				</section>
 			</main>
