6c48256683260045d18e371d9f022e26fdec37d8
Kwabena Owusu-Darko <kobbyowusudarko@gmail.com>
2024-01-24 18:33:39 +0000

    Added localStorageApi helpers

--- frontend/utils/helpers.tsx
+++ frontend/utils/helpers.tsx
@@ -23,3 +23,28 @@ export function resetDarkMode(){
 export function isEmpty(obj: any) {
     return Object.keys(obj).length === 0;
 }
+
+
+export function getProfileStocks(): any[]{
+    let ls = localStorage.getItem('ps');
+    if (ls == null) return []
+
+    return JSON.parse(ls)
+}
+
+export function existsInProfile(callSign: string): boolean {
+    let existing = getProfileStocks()
+    return existing.includes(callSign)
+}
+
+export function addStock(callSign: string){
+    let existing = getProfileStocks();
+    existing.push(callSign) 
+    localStorage.setItem('ps',JSON.stringify(existing))
+}
+
+export function removeStock(callSign: string){
+    let existing = getProfileStocks();
+    let newArr = existing.filter((stock) => {return stock != callSign} ) 
+    localStorage.setItem('ps',JSON.stringify(newArr))
+}
